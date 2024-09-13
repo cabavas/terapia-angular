82e5bfde20512145b4d8d77cab017f8db4b598c5
cabavas <cabavas@gmail.com>
2024-09-13 12:17:45 -0300

    header completed and responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PatientsComponent } from './pages/patients/patients.component';
+import { SessionsComponent } from './pages/sessions/sessions.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
-    { path: "login", component: LoginComponent}
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: "login", component: LoginComponent },
+    { path: "patients", component: PatientsComponent },
+    { path: "sessions", component: SessionsComponent },
+    { path: "reports", component: ReportsComponent },
+    { path: "profile", component: ProfileComponent },
+    {path: '**', redirectTo: ''},
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) { }
+
+  navigate(): void {
+    this.router.navigate(['/home']);
+  }
+
+  logout() {
+
+  }
 }
